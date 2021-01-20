6fec0481c85084dcf2212345a5fc17807bc3cbf9
Cristiano <cristiano@gehring.gehring>
2021-01-20 20:39:13 -0300

    First Commit: Project structure created

---
+++ src/main/java/com/cristianogehring/bookstroremanager/BookstroremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.cristianogehring.bookstroremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstroremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstroremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristianogehring/bookstroremanager/BookstroremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianogehring.bookstroremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstroremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
